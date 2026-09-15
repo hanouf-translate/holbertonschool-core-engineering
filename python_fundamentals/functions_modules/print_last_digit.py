eb2dac99626680c8822a2401770b95958cd8145e
hanouf-translate <hanouf-translate>
2026-09-15 11:41:54 +0200

    last digit

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -3,8 +3,8 @@
 
 
 def print_last_digit(number):
-    if number < 0:
-        last_digit = -(-number % 10)
-    else:
-        last_digit = number % 10
-    print("{}".format(last_digit), end="")
+  if number < 0:
+    last_digit = (-number % 10)
+  else:
+    last_digit = number % 10
+  return last_digit
