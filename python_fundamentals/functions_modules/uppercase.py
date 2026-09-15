b439c7cccfd131d231349d647402b2988543358a
hanouf-translate <hanouf-translate>
2026-09-15 09:23:39 +0200

    Uppercase

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def uppercase(str):
+    for i in str:
+        if ord('a') <= ord(i) <= ord('z'):
+            print(chr(ord(i) - 32), end="")
+        else:
+            print(i, end="")
+    print()
