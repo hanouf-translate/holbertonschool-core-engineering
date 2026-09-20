a270554a787f8365fc1c4004d81fec4029c96795
hanouf-translate <hanouf-translate>
2026-09-20 14:00:17 +0300

    Write a function that divides two integers.

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 def safe_print_division(a, b):
     try:
         result = a / b
@@ -9,9 +8,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
     return result
-
-
-# if __name__ == "__main__":
-#     safe_print_division(10, 2)
-#     safe_print_division(10, 0)
-#     safe_print_division(10, 5)
