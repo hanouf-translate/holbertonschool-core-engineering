8977bdfe511e1862bb4238bc50d769cb07fff737
hanouf-translate <hanouf-translate>
2026-09-21 12:57:14 +0300

    print square instance

--- python_oop/6-square.py
+++ python_oop/6-square.py
@@ -50,6 +50,9 @@ class Square:
             print()
 
         else:
+            for _ in range(self.__position[1]):
+                print()
+                
             for _ in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
