107ae235bdd1aa43e70153a795a100b1a89ec265
hanouf-translate <hanouf-translate>
2026-09-21 10:57:03 +0300

    first class

--- python_oop/1-square.py
+++ python_oop/1-square.py
@@ -8,6 +8,15 @@ This module defines a Square class.
 class Square:
     """Defines a square."""
     def __init__(self,size):
+        try:
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        except TypeError as e:
+            print(e)
+        except ValueError as e:
+            print(e)
         """Initializes a square with a given size.
 
         Args:
