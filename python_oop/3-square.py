1bbce799bd467f1c1572ec7bd199a3ed25ac4203
hanouf-translate <hanouf-translate>
2026-09-21 11:11:23 +0300

    first class

---
+++ python_oop/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+1-square module
+
+This module defines a Square class with an Area method.
+"""
+
+class Square:
+    """Defines a square."""
+    def __init__(self,size=0):
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        """Initializes a square with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+    def area(self):
+        """calculated an area of a square"""
+        return self.__size ** 2
