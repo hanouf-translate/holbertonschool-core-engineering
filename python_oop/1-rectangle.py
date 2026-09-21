7ef5eaf032b22001a9e9148383d4be9f1959668f
hanouf-translate <hanouf-translate>
2026-09-21 14:06:52 +0300

    class rectangle

--- python_oop/1-rectangle.py
+++ python_oop/1-rectangle.py
@@ -51,3 +51,12 @@ class Rectangle:
 
     height = property(height, __set_height)
     width = property(width, __set_width)
+    def area(self):
+        """Calculates the area of the rectangle"""
+        return self.__width * self.__height
+        
+    def perimeter(self):
+        """Calculates the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
