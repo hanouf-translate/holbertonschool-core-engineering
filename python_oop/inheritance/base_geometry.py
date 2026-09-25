fd96fdb654870b9b9675b7847a4a7e55223fc2d6
hanouf-translate <hanouf-translate>
2026-09-25 17:19:28 +0300

    Create a class named BaseGeometry.

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-
+"""
+Module base_geometry
+Defines a BaseGeometry class.
+"""
 class BaseGeometry:
+    """A BaseGeometry class."""
     def area(self):
-        raise Exception(" not implemented in this class.")
+        raise Exception("not implemented in this class.")
     
     def integer_validator(self, name, value):
         if not isinstance(value, int):
