6babe6324d3e92f0ed1a555c2582159d185478c1
hanouf-translate <hanouf-translate>
2026-09-25 17:52:34 +0300

    Create a class named Rectangle.

--- python_oop/inheritance/2-rectangle.py
+++ python_oop/inheritance/2-rectangle.py
@@ -15,6 +15,13 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height" , height)
         self.__height = height
     
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.__width * self.__height
+        
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
         
         
 
