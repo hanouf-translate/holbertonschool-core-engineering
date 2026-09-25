4b7b191ff6efb573469c4a213e89b7019d818a2b
hanouf-translate <hanouf-translate>
2026-09-25 19:29:40 +0300

    Create a class named Rectangle.

---
+++ python_oop/inheritance/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module square
+Defines a square class.
+"""
+Rectangle = __import__('base_geometry').Rectangle
+
+class Square(Rectangle):
+    def __init__(self, size):
+
+        self.vainteger_validator("size", size)
+        self.__size = size
+        # super().__init__(size, size) 
+    
+    def area(self):
+        return self.__size ** 2
