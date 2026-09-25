93c7992d86ef2b1dc049ba1c42cacf600adad793
hanouf-translate <hanouf-translate>
2026-09-25 19:38:21 +0300

    Create a class named Rectangle.

--- python_oop/inheritance/1-square.py
+++ python_oop/inheritance/1-square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     """A square class."""
     def __init__(self, size):
 
-        self.vainteger_validator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
         # super().__init__(size, size) 
     
