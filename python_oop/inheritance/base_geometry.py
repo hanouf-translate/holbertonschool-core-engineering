2efd8d0d5bf304138db2a3e60a4c2d5e706266a3
hanouf-translate <hanouf-translate>
2026-09-25 17:23:23 +0300

    Create a class named BaseGeometry.

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -6,10 +6,10 @@ Defines a BaseGeometry class.
 class BaseGeometry:
     """A BaseGeometry class."""
     def area(self):
-        raise Exception("not implemented in this class.")
+        raise Exception("area() is not implemented.")
     
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
-        if value < 0:
+        if value =< 0:
             raise ValueError(f"{name} must be greater than 0")
