19c9306619bc1c2e70893691d1db457627c4002b
hanouf-translate <hanouf-translate>
2026-09-25 17:36:54 +0300

    Create a class named Rectangle.

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -8,8 +8,11 @@ BaseGeometry = __import__('base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """A rectangle class."""
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         BaseGeometry.integer_validator(width , width)
+        self.__width = width
+        
         BaseGeometry.integer_validator(height , height)
+        self.__height = height
+        
+        
 
