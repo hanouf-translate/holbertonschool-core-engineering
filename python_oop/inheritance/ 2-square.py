0dd6e49f7fd01299082cff209245487e97403719
hanouf-translate <hanouf-translate>
2026-09-25 19:40:03 +0300

    Create a class named Rectangle.

---
+++ python_oop/inheritance/ 2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module square
+Defines a square class.
+"""
+Rectangle = __import__('2-rectangle').Rectangle
+
+class Square(Rectangle):
+    """A square class."""
+    def __init__(self, size):
+
+        self.integer_validator("size", size)
+        self.__size = size
+        # super().__init__(size, size) 
+    
+    def area(self):
+        return self.__size ** 2
+    
+    def __str__(self):
+        """Return a string representation of the square."""
+        return f"[Square] {self.__size}/{self.__size}"
